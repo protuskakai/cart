8a2767a3b7eff34c09b2f9e3f5e10850a8bf83d3
Protus Kakai <protuskakai@yahoo.com>
2023-07-24 17:31:24 +0300

    version4

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,7 +5,7 @@
           <main class="my-8">
             <div class="container px-6 mx-auto">
                 <div class="flex justify-center my-6">
-                    <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+                    <div class="flex flex-col w-full p-8 text-gray-800  bg-green-100 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
                       @if ($message = Session::get('success'))
                           <div class="p-4 mb-3 bg-green-400 rounded">
                               <p class="text-green-800">{{ $message }}</p>
@@ -28,7 +28,8 @@
                           </thead>
                           <tbody>
                               @foreach ($cartItems as $item)
-                            <tr>
+                            <tr style="border-bottom:1px solid blue" >
+							
                               <td class="hidden pb-4 md:table-cell">
                                 <a href="#">
                                   <img src="./images/{{ $item->attributes->image }}" class="w-20 rounded" alt="Thumbnail">
@@ -65,13 +66,19 @@
                                   <input type="hidden" value="{{ $item->id }}" name="id">
                                   <button class="px-4 py-2 text-white bg-red-600">x</button>
                               </form>
-                                
+                               
                               </td>
+							  
                             </tr>
+							<tr><td></td> </tr>
+							<tr><td></td> </tr>
+							<tr><td></td> </tr>
+							
                             @endforeach
                              
                           </tbody>
                         </table>
+						
                         <div >
                          Total: Ksh {{ Cart::getTotal() }}
 						 <br>
